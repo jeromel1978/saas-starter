8566bbed1898ffb0e7be25625aad7646e576459d
jeromel1978 <jeromelosorata@hotmail.com>
2026-08-12 05:14:34 -0500

    Updated Pricing page to work off of Stripe catalog

--- app/(dashboard)/pricing/page.tsx
+++ app/(dashboard)/pricing/page.tsx
@@ -1,7 +1,7 @@
-import { checkoutAction } from '@/lib/payments/actions';
-import { Check } from 'lucide-react';
-import { getStripePrices, getStripeProducts } from '@/lib/payments/stripe';
-import { SubmitButton } from './submit-button';
+import { checkoutAction } from "@/lib/payments/actions";
+import { Check } from "lucide-react";
+import { getStripePrices, getStripeProducts } from "@/lib/payments/stripe";
+import { SubmitButton } from "./submit-button";
 
 // Prices are fresh for one hour max
 export const revalidate = 3600;
@@ -12,39 +12,29 @@ export default async function PricingPage() {
     getStripeProducts(),
   ]);
 
-  const basePlan = products.find((product) => product.name === 'Base');
-  const plusPlan = products.find((product) => product.name === 'Plus');
-
-  const basePrice = prices.find((price) => price.productId === basePlan?.id);
-  const plusPrice = prices.find((price) => price.productId === plusPlan?.id);
-
+  console.log("Prices", prices);
+  console.log("Products", products);
   return (
     <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="grid md:grid-cols-2 gap-8 max-w-xl mx-auto">
-        <PricingCard
-          name={basePlan?.name || 'Base'}
-          price={basePrice?.unitAmount || 800}
-          interval={basePrice?.interval || 'month'}
-          trialDays={basePrice?.trialPeriodDays || 7}
-          features={[
-            'Unlimited Usage',
-            'Unlimited Workspace Members',
-            'Email Support',
-          ]}
-          priceId={basePrice?.id}
-        />
-        <PricingCard
-          name={plusPlan?.name || 'Plus'}
-          price={plusPrice?.unitAmount || 1200}
-          interval={plusPrice?.interval || 'month'}
-          trialDays={plusPrice?.trialPeriodDays || 7}
-          features={[
-            'Everything in Base, and:',
-            'Early Access to New Features',
-            '24/7 Support + Slack Access',
-          ]}
-          priceId={plusPrice?.id}
-        />
+        {products.map((price) => (
+          <PricingCard
+            key={price.id}
+            name={price?.name || "Base"}
+            price={0}
+            interval="once"
+            trialDays={0}
+            // price={price?.unitAmount || 800}
+            // interval={price?.interval || "month"}
+            // trialDays={price?.trialPeriodDays || 7}
+            features={[
+              "Unlimited Usage",
+              "Unlimited Workspace Members",
+              "Email Support",
+            ]}
+            priceId={price?.id}
+          />
+        ))}
       </div>
     </main>
   );
@@ -72,7 +62,7 @@ function PricingCard({
         with {trialDays} day free trial
       </p>
       <p className="text-4xl font-medium text-gray-900 mb-6">
-        ${price / 100}{' '}
+        ${price / 100}{" "}
         <span className="text-xl font-normal text-gray-600">
           per user / {interval}
         </span>
